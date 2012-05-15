161a195aaa715d8cf460724bbbf9250eb7df8263
zomtec <timo.behrmann@gmail.com>
2012-05-16 02:06:34 +0700

    Place and edit bets

--- src/main/webapp/js/bet.js
+++ src/main/webapp/js/bet.js
@@ -2,27 +2,16 @@ function isNumber(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function placeBet(queryString) {
-	$.ajax({
-		type : 'POST',
-		url : '../bet',
-		data : queryString,
-		success : function(data) {
-			if (data == "true") {
-				alert("saved");
-			} else {
-				alert("Sorry, please try again later.");
-			}
-		},
-		error : function() {
-			alert("Sorry, please try again later.");
-		},
-		dataType : 'text'
-	});
-};
-
 $(document).bind("mobileinit", function() {
 	$.mobile.page.prototype.options.addBackBtn = true;
+	
+	$("a.editBet").live("click", function(e){
+		e.stopImmediatePropagation();
+		e.preventDefault();
+		var li = $(this).parent().parent();
+		li.find("div.placeBetContainer").show();
+		li.find("div.readBetContainer").hide();
+	});
 	
 	$("a.placeBet").live("click", function(e){ 
 		e.stopImmediatePropagation();
@@ -44,7 +33,25 @@ $(document).bind("mobileinit", function() {
 		});
 		
 		if (!error) {
-			placeBet(queryString);
+			$.ajax({
+				type : 'POST',
+				url : '../bet',
+				data : queryString,
+				success : function(data) {
+					if (data == "true") {
+						var li = form.parent().parent();
+						var scoreString = form.children("input[name=home]").val() + " : " + form.children("input[name=away]").val();
+						li.find("div.placeBetContainer").hide();
+						li.find("div.readBetContainer").show().find("span.readOnlyBetScore").text(scoreString);
+					} else {
+						alert("Sorry, please try again later.");
+					}
+				},
+				error : function() {
+					alert("Sorry, please try again later.");
+				},
+				dataType : 'text'
+			});
 		}
 	});   
 });
