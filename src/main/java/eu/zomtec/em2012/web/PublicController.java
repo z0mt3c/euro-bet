20a626494fb0bda7f25f4a87eb7988ecc91fac18
zomtec <timo.behrmann@gmail.com>
2012-05-18 01:26:01 +0700

    test

--- src/main/java/eu/zomtec/em2012/web/PublicController.java
+++ src/main/java/eu/zomtec/em2012/web/PublicController.java
@@ -1,12 +1,17 @@
 package eu.zomtec.em2012.web;
 
+import java.security.Principal;
 import java.util.List;
 
+import javax.persistence.TypedQuery;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import eu.zomtec.em2012.domain.BetUser;
 import eu.zomtec.em2012.domain.News;
 import eu.zomtec.em2012.score.HighScore;
 import eu.zomtec.em2012.score.HighScoreService;
@@ -19,10 +24,29 @@ public class PublicController {
 	private HighScoreService highScoreService;
 	
 	@RequestMapping(value={"/","/news"})
-	public String news(ModelMap modelMap) {
+	public String news(ModelMap modelMap, Principal principal) {
 		final List<News> news = News.findNewsEntries(10);
 		modelMap.put("news", news);
+		
+		final List<HighScore> highScores = highScoreService.getHighScoreTemp();
+		modelMap.put("scores_temp", highScores);
+		
+		final BetUser user = getUser(principal);
+		if (user != null) {
+			modelMap.put("scores_temp_my", highScoreService.getHighScorePartForUser(user.getId(), 2, highScores));
+		}
+		
 		return "public/news";
+	}
+	
+	private BetUser getUser(Principal principal) {
+		if (principal == null || StringUtils.isBlank(principal.getName())) {
+			return null;
+		}
+		
+		final TypedQuery<BetUser> userQuery = BetUser.findBetUsersByUsernameEquals(principal.getName());
+    	final BetUser user = userQuery.getSingleResult();
+		return user;
 	}
 	
     @RequestMapping(value="/scores")
